4ff983dc089f20695bae93e4936511537bc26c40
Suzana Melo <suzana@conqahq.com>
2022-07-06 19:35:01 +1200

    suzana: move state logic to the parent

--- src/components/WebcamCapture.js
+++ src/components/WebcamCapture.js
@@ -1,56 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 import Webcam from "react-webcam";
 
 const videoConstraints = {
-    width: 1280,
-    height: 720,
-    facingMode: "user"
+  width: 1280,
+  height: 720,
+  facingMode: "user",
 };
 
-
-const WebcamCapture = () => {
-    const [image, setImage] = useState("");
-    const [imageList, setList] = useState([]);
-
-    console.log("imageList", imageList);
-
-    const saveCapture = (im) => {
-        imageList.push(im);
-        setList(imageList);
-    };
-
-    return (
-        <>
-            <Webcam
-                audio={false}
-                height={720}
-                screenshotFormat="image/jpeg"
-                width={1280}
-                videoConstraints={videoConstraints}
-            >
-            {({ getScreenshot }) => (
-            <button
-                onClick={() => {
-                    const imageSrc = getScreenshot()
-                    saveCapture(imageSrc);
-                    setImage(imageSrc);
-                    //save image to an array
-                }}
-            >
-                Capture photo
-            </button>
-            )}
-            </Webcam>
-            {/* map to render each image */}
-            {
-                imageList.map((im) => {
-                    return (
-                        <img alt="test" src={im} />
-                    );
-                })
-            }
-        </>
-    );
+const WebcamCapture = ({ handleClick }) => {
+  return (
+    <>
+      <Webcam
+        audio={false}
+        height={720}
+        screenshotFormat="image/jpeg"
+        width={1280}
+        videoConstraints={videoConstraints}
+      >
+        {({ getScreenshot }) => (
+          <button
+            onClick={() => {
+              const imageSrc = getScreenshot();
+              handleClick(imageSrc);
+            }}
+          >
+            Capture photo
+          </button>
+        )}
+      </Webcam>
+    </>
+  );
 };
 
 export default WebcamCapture;
