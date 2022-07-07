9755a5b322853970d3fbc4ae561e44c08843230d
cml35 <claudynn@conqahq.com>
2022-07-07 14:56:39 +1200

    Upload images to S3

--- src/components/AddImages/AddImages.js
+++ src/components/AddImages/AddImages.js
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
+import uploadFile, { setConfig } from "@conqa/s3-upload/dist";
+
 import "./AddImages.css";
 import WebcamCapture from "../WebcamCapture/WebcamCapture";
+// import toFile from "../../utils";
+
+setConfig({
+  // env should be set to one of 'staging' or 'production'
+  env: "staging"
+});
 
 const Home = () => {
+  
   const [image, setImage] = useState("");
   const [title, setTitle] = useState("");
   const [bulkImages, setBulkImages] = useState([]);
@@ -20,6 +29,38 @@ const Home = () => {
     setTitle("");
   };
 
+  const transformDataURLToFile = (dataURL, fileName) => {
+    // convert base64 to raw binary data held in a string
+  // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+  let byteString = atob(dataURL.split(',')[1]);
+
+  // separate out the mime component
+  let mimeString = dataURL.split(',')[0].split(':')[1].split(';')[0]
+
+  // write the bytes of the string to an ArrayBuffer
+  let ab = new ArrayBuffer(byteString.length);
+
+  // create a view into the buffer
+  let ia = new Uint8Array(ab);
+
+  // set the bytes of the buffer to the correct values
+  for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+  }
+
+  // write the ArrayBuffer to a blob, and you're done
+  let file = new File([ab], fileName, {type: mimeString});
+  return file;
+  }
+
+  const transform = (bulkImages) => {
+    console.log("@@@@@bulkImages", bulkImages);
+    const transformedImages = bulkImages.map((img) => {
+      return transformDataURLToFile(img.image, img.title);
+    });
+    return transformedImages;
+  };
+
   const submitTitle = (e) => {
     e.preventDefault();
     createBulkImage();
@@ -31,8 +72,16 @@ const Home = () => {
     setShowForm(true);
   };
 
-  const uploadPhoto = () => {
+  const upload = (bulkImages) => {
+    const files = transform(bulkImages);
+    console.log("files", files);
+
     //Uploads the bulk of images to sS3
+    files.map(async (file) => {
+      const result = await uploadFile(file);
+      console.log('result', result);
+      return result;
+    });
   };
 
   return (
@@ -69,7 +118,7 @@ const Home = () => {
       {bulkImages.length > 0 && (
         <button
           onClick={() => {
-            uploadPhoto();
+            upload(bulkImages);
           }}
         >
           {" "}
